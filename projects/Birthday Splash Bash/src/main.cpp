82e97d4a07aa724df26270d5b7faba939422569f
Alexander Chow <alexander.chow@ontariotechu.net>
2020-10-14 14:50:29 -0400

    Working window

--- projects/Birthday Splash Bash/src/main.cpp
+++ projects/Birthday Splash Bash/src/main.cpp
@@ -1,5 +1,56 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <GLM/glm.hpp>
+
+//initializes the window
+GLFWwindow* firstWindow;
+
+bool GLFW()
+{
+	//checks if glfw died
+	if (glfwInit() == GLFW_FALSE)
+	{
+		std::cout << "GLFW fail" << std::endl;
+		return false;
+	}
+	//								size			Title			 monitor?  share?
+	firstWindow = glfwCreateWindow(500, 500, "Birthday Splash Bash", nullptr, nullptr);
+	glfwMakeContextCurrent(firstWindow);
+
+	return true;
+}
+
+bool GLAD()
+{
+	//checks if glad died
+	if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) == 0)
+	{
+		std::cout << "glad failed" << std::endl;
+		return false;
+	}
+}
 
 int main()
 {
-	return 0; 
+	//initialize GLFW
+	if (!GLFW())
+		return -1;
+
+	//initialize GLAD
+	if (!GLAD())
+	{
+		return -1;
+	}
+
+	//Game loop/makes the window//
+	while (!glfwWindowShouldClose(firstWindow))
+	{
+		//lets the user click on the window, maybe?
+		glfwPollEvents();
+
+		glfwSwapBuffers(firstWindow);
+	}
+
+	return 0;
 } 
